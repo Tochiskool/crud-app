b7549eea22f647731c24ac6b899c6dfcf0927c4a
Tochiskool <mrchristian79@yahoo.com>
2024-05-19 14:39:41 +0200

    Crud operations completed

--- client/src/components/Users/index.jsx
+++ client/src/components/Users/index.jsx
@@ -17,6 +17,16 @@ const Users = () => {
       .then((result) => setUsers(result.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:5001/deleteUser/" + id)
+      .then((res) => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
@@ -33,23 +43,27 @@ const Users = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, index) => {
+            {users.map((user) => {
               return (
-                <>
-                  <tr key={index}>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.age}</td>
-                    <td>
-                      <Link to='/update' className='btn btn-success'>
-                        Update
-                      </Link>
-                      <Link to='/delete' className='btn btn-danger'>
-                        Delete
-                      </Link>
-                    </td>
-                  </tr>
-                </>
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>
+                    <Link
+                      to={`/update/${user._id}`}
+                      className='btn btn-success'
+                    >
+                      Update
+                    </Link>
+                    <button
+                      className='btn btn-danger'
+                      onClick={() => handleDelete(user._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
               );
             })}
           </tbody>
