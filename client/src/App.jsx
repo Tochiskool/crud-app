76063e34ebf265f6bece188e731354c887da342b
Tochiskool <mrchristian79@yahoo.com>
2024-05-19 12:32:47 +0200

    updates made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateUser from "./components/CreateUser";
 import UpdateUser from "./components/UpdateUser";
-import User from "./components/User";
+import User from "./components/Users";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <BrowserRouter>
